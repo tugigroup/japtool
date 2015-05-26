435105c596da5f56d5a3864f22b88b000120e8c9
tugigroup <xbar.usui@gmail.com>
2015-05-26 18:47:59 +0900

    Update connections.js
    edit conflig

--- config/connections.js
+++ config/connections.js
@@ -56,7 +56,7 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  taittMongodbServer: {
+  someMongodbServer: {
     adapter: 'sails-mongo',
     host: 'localhost',
     port: 27017,
