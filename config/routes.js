a2f8333fa08c69909170a3589f19d7e2e8f23f07
duyquyet8886 <QuyetND2@117QuyetND2.fsoft.fpt.vn>
2015-05-27 16:21:29 +0700

    merge code

--- config/routes.js
+++ config/routes.js
@@ -22,32 +22,31 @@
 
 module.exports.routes = {
 
-  /***************************************************************************
-  *                                                                          *
-  * Make the view located at `views/index.ejs` (or `views/homepage.jade`, *
-  * etc. depending on your default view engine) your home page.              *
-  *                                                                          *
-  * (Alternatively, remove this and add an `index.html` file in your         *
-  * `assets` directory)                                                      *
-  *                                                                          *
-  ***************************************************************************/
+    /***************************************************************************
+     *                                                                          *
+     * Make the view located at `views/index.ejs` (or `views/homepage.jade`, *
+     * etc. depending on your default view engine) your home page.              *
+     *                                                                          *
+     * (Alternatively, remove this and add an `index.html` file in your         *
+     * `assets` directory)                                                      *
+     *                                                                          *
+     ***************************************************************************/
 
-  '/': {
-    view: 'index'
-  },
-  'get /tranning': 'QuestionController.showAction',
-  '/tranningindex': {view: 'trainning_index'},
-  'post /confirm': 'QuestionController.confirmAction'
-  }
+    '/': {
+        view: 'index'
+    },
+    'get /tranning': 'QuestionController.showAction',
+    '/tranningindex': {view: 'trainning_index'},
+    'post /confirm': 'QuestionController.confirmAction'
 
-  /***************************************************************************
-  *                                                                          *
-  * Custom routes here...                                                    *
-  *                                                                          *
-  * If a request to a URL doesn't match any of the custom routes above, it   *
-  * is matched against Sails route blueprints. See `config/blueprints.js`    *
-  * for configuration options and examples.                                  *
-  *                                                                          *
-  ***************************************************************************/
+/***************************************************************************
+ *                                                                          *
+ * Custom routes here...                                                    *
+ *                                                                          *
+ * If a request to a URL doesn't match any of the custom routes above, it   *
+ * is matched against Sails route blueprints. See `config/blueprints.js`    *
+ * for configuration options and examples.                                  *
+ *                                                                          *
+ ***************************************************************************/
 
 };
