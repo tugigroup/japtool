cb3363214787f0b676c8e8affbcf271ca747075f
unknown <TuyenTV1@117TuyenTV1.fsoft.fpt.vn>
2015-05-29 16:49:57 +0700

    Update 29.5

--- api/models/User.js
+++ api/models/User.js
@@ -31,21 +31,53 @@ module.exports = {
         encryptedPassword: {
             type: 'string'
         },
-
-        address: {
-            type: 'string'
-        },
         admin: {
             type: 'boolean',
             defaultsTo: false
+        },
+        address: {
+            type: 'string'
+        },
+        gender: {
+            type: 'integer'
+        },
+        birthday: {
+            type: 'date'
+        },
+        website: {
+            type: 'string'
+        },
+        avatar: {
+            type: 'string'
+        },
+        status: {
+            type: 'boolean',
+            defaultsTo: true
+        },
+        lastLogin: {
+            type: 'date'
+        },
+        creatingDate: {
+            type: 'date'
+        },
+        online: {
+            type: 'boolean',
+            defaultsTo: false
+        },
+        // Add a reference to User
+        buddyGroup: {
+            collection: "buddyGroup",
+            via: "users"
         }
+
+
     },
     beforeCreate: function (values, next) {
         //This checks to make sure the password confirmation match before creating record
         if (!values.password || values.password != values.confirmation) {
             return next({err: ["Password doesn't match password confirmation."]});
         }
-        require('bcrypt').hash(values.password, 10, function passwordEncypted(err, encryptedPassword) {
+        require('bcryptjs').hash(values.password, 10, function passwordEncypted(err, encryptedPassword) {
             if (err) {
                 return next(err);
             }
