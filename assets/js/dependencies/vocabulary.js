ca1785e1691c2927f8a6a088b933968c2a39420b
duluong <luongvandu@gmail.com>
2015-06-07 23:55:48 +0900

    show cartagame style

--- assets/js/dependencies/vocabulary.js
+++ assets/js/dependencies/vocabulary.js
@@ -2,4 +2,5 @@ $(document).ready(function () {
     $('.flashcard').load('/vocabulary/flashcard');
     $('.listVocabulary').load('/vocabulary/list');
     $('.quicklearning').load('/vocabulary/quicklearning');
+    $('.cartagame').load('/cartagame/wordgen');
 });
