a065caf456b643832cf239b1c9c77fcb2294cc10
duluong <luongvandu@gmail.com>
2015-06-23 23:20:18 +0900

    up

---
+++ importBook.js
@@ -0,0 +1,132 @@
+var fs = require('fs');
+var parse = require('csv-parse');
+var parser = parse({delimiter : ','});
+
+var mongoose = require('mongoose');
+var config = require('./config.json');
+
+data = fs.readFileSync(config.book_csv_file,{"encoding":"utf8"});
+//console.log(data);
+
+parse(data, {delimiter : ',', comment: '#'}, function(err, lessions){
+
+	// check header
+	if (lessions.length == 0){
+		console.log('Error! csv file has not any data.');
+	}
+
+	var header = lessions[0];
+	if (header.length < 13 || 
+		header[0] != 'name' ||
+		header[1] != 'description' ||
+		header[2] != 'type' ||
+		header[3] != 'level' ||
+		header[4] != 'lessionNum' ||
+		header[5] != 'hoursForLearn' ||
+		header[6] != 'usedNum' ||
+		header[7] != 'recommendNum' ||
+		header[8] != 'lession' ||
+		header[9] != 'subLession' ||
+		header[10] != 'useModule' ||
+		header[11] != 'useCollection' ||
+		header[12] != 'dataExtractCondition' ){
+
+		console.log('Error! Format of csv file is not correct.');
+	}
+
+	// connect to mongodb
+	mongoose.connect('mongodb://' + config.dbhost + '/' + config.database);
+
+	var bookMasterSchema = mongoose.Schema({
+	  name:  String,
+	  description: String,
+	  type:   String,
+	  level:   String,
+	  lessionNum:   Number,
+	  hoursForLearn:   Number,
+	  usedNum: Number,
+	  recommendNum:   Number
+	},{ collection: 'bookMaster', versionKey: false });
+
+	var bookMasterColl = mongoose.model('bookMaster', bookMasterSchema);
+
+	var bookDetailSchema = mongoose.Schema({
+	  bookID:  mongoose.Schema.Types.ObjectId,
+	  lession: String,
+	  subLession: String,
+	  useModule: String,
+	  useCollection: String,
+	  dataExtractCondition:   String
+	},{ collection: 'bookDetail', versionKey: false  });
+
+	var bookDetailColl = mongoose.model('bookDetail', bookDetailSchema);
+
+	var lession;
+	var bookMasterInsertCount = 0;
+	var bookDetailInsertCount = 0;
+	var errorCount = 0;
+	var insertedBookMaser;
+	var insertedBookDetail;
+
+	for ( var i = 1; i < lessions.length; i++ ) {
+		//console.log(lessions[i]);
+
+		lession = lessions[i];
+
+		if (lession[0] == "") {
+			if (insertedBookMaser == null){
+				errorCount++;
+				continue; 
+
+			} else{
+				// insert book detail record
+				insertedBookDetail = new bookDetailColl({
+					bookID:  				insertedBookMaser._id,
+				    lession: 				lession[8],
+				    subLession: 			lession[9],
+				    useModule: 				lession[10],
+				    useCollection: 			lession[11],
+				    dataExtractCondition: 	lession[12]
+				});
+
+				insertedBookDetail.save();
+				bookDetailInsertCount++;
+			}
+		} else {
+			// insert book master record
+			insertedBookMaster = new bookMasterColl({
+				name:  			lession[0],
+				description: 	lession[1],
+				type:   		lession[2],
+				level:   		lession[3],
+				lessionNum:   	lession[4],
+				hoursForLearn:  lession[5],
+				usedNum: 		lession[6],
+				recommendNum:   lession[7]
+			});
+
+			insertedBookMaster.save();
+			bookMasterInsertCount++;
+
+			// insert book detail record
+			insertedBookDetail = new bookDetailColl({
+				bookID:  				insertedBookMaser._id,
+			    lession: 				lession[8],
+			    subLession: 			lession[9],
+			    useModule: 				lession[10],
+			    useCollection: 			lession[11],
+			    dataExtractCondition: 	lession[12]
+			});
+
+			insertedBookDetail.save();
+			bookDetailInsertCount++;
+		}
+	};
+
+	console.log('bookMaser collection: ' + bookMasterInsertCount.toString() + ' records was inserted.');
+	console.log('bookDetail collection: ' + bookDetailInsertCount.toString() + ' records was inserted.');
+	console.log('Error records: ' + errorCount.toString() );
+
+	// disconect mongodb
+	mongoose.disconnect();
+});
---
+++ importVocabulary.js
@@ -0,0 +1,107 @@
+var fs = require('fs');
+var parse = require('csv-parse');
+var parser = parse({delimiter : ','});
+
+var mongoose = require('mongoose');
+var config = require('./config.json');
+
+data = fs.readFileSync(config.vocabulary_csv_file,{"encoding":"utf8"});
+//console.log(data);
+
+parse(data, {delimiter : ',', comment: '#'}, function(err, vocabularies){
+
+	// check header
+	if (vocabularies.length == 0){
+		console.log('Error! csv file has not any data');
+	}
+
+	var header = vocabularies[0];
+	if (header.length < 8 || 
+		header[0] != 'item' ||
+		header[1] != 'reading' ||
+		header[2] != 'description' ||
+		header[3] != 'example' ||
+		header[4] != 'level' ||
+		header[5] != 'sort' ||
+		header[6] != 'tag' ||
+		header[7] != 'category' ){
+
+		console.log('Error! Format of csv file is not correct.');
+	}
+
+	// connect to mongodb
+	mongoose.connect('mongodb://' + config.dbhost + '/' + config.database);
+
+	var vocabularySchema = mongoose.Schema({
+	  item:  String,
+	  reading: String,
+	  description:   String,
+	  level:   String,
+	  sort:   Number,
+	  tag:   String,
+	  category:   String
+	},{ collection: 'vocabulary', versionKey: false });
+
+	var vocabularyColl = mongoose.model('vocabulary', vocabularySchema);
+
+	var exampleSchema = mongoose.Schema({
+	  exampleSetID:  mongoose.Schema.Types.ObjectId,
+	  example: String,
+	  meaning:   String
+	},{ collection: 'example', versionKey: false  });
+
+	var exampleColl = mongoose.model('example', exampleSchema);
+
+
+	var vocabulary, tmpString, jsonVocab;
+
+	var vocabInsertCount = 0;
+	var exampleInsertCount = 0;
+
+	for ( var i = 1; i < vocabularies.length; i++ ) {
+		//console.log(vocabularies[i]);
+
+		vocabulary = vocabularies[i];
+
+		// tmpString = "{\"" + header[0] + "\":\"" + vocabulary[0] + "\",\"" +
+		// 					header[1] + "\":\"" + vocabulary[1] + "\",\"" +
+		// 					header[2] + "\":\"" + vocabulary[2] + "\",\"" +
+		// 					header[4] + "\":\"" + vocabulary[4] + "\",\"" +
+		// 					header[5] + "\":\"" + vocabulary[5] + "\",\"" +
+		// 					header[6] + "\":\"" + vocabulary[6] + "\",\"" +
+		// 					header[7] + "\":\"" + vocabulary[7] + "\"}" ;
+
+		// jsonVocab = JSON.parse(tmpString);
+
+		var insertedVocab = new vocabularyColl({
+			item:  		vocabulary[0],
+			reading: 	vocabulary[1],
+			description:vocabulary[2],
+			level:   	vocabulary[4],
+			sort:   	vocabulary[5],
+			tag:   		vocabulary[6],
+			category:   vocabulary[7]
+		});
+		
+		insertedVocab.save();
+		vocabInsertCount ++;
+
+		examples = vocabulary[3].toString().split(config.chars_split_between_examples);
+		examples.forEach(function(example) {
+			items = example.split(config.chars_split_insite_examples);
+			var insertedExample = new exampleColl({ exampleSetID: insertedVocab._id,
+													example: items[0],
+													meaning: items[1] 
+												});
+
+			insertedExample.save();
+			exampleInsertCount++;
+		});
+	};
+
+	console.log('vocabulary collection: ' + vocabInsertCount.toString() + ' records was inserted.');
+	console.log('example collection: ' + exampleInsertCount.toString() + ' records was inserted.');
+
+	// disconect mongodb
+	mongoose.disconnect();
+});
