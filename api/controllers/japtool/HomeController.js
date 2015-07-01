c62265a721068058420fca22cc8c5a96941f260e
duyquyet8886 <QuyetND2@117QuyetND2.fsoft.fpt.vn>
2015-07-01 10:30:32 +0700

    delete source

--- api/controllers/japtool/HomeController.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by TuyenTV1 on 6/22/2015.
- */
-module.exports = {
-//This loads index.ejs
-    index: function(req, res){
-        res.view({ layout: 'layout/japtool-home'});
-    }
-}
