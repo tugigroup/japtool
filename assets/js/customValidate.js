d015ca853d1d42731c5b541bcca091f1ee44991d
unknown <TuanNT22@117TuanNT22.fsoft.fpt.vn>
2015-07-02 17:20:52 +0700

    Update custom validate in user edit info

--- assets/js/customValidate.js
+++ assets/js/customValidate.js
@@ -1,45 +1,45 @@
 $(document).ready(function () {
-    $('.form-register').validate({
-        rules: {
-            username: {
-                required: true
-            },
-            firstname: {
-                required: true
-            },
-            lastname: {
-                required: true
-            },
-            email: {
-                required: true,
-                email: true
-            },
-            password: {
-                minlength: 6,
-                required: true
-            },
-            confirmation: {
-                minlength: 6,
-                equalTo: "#input-password"
-            }
-        }
-    });
+  $('.form-register').validate({
+    rules: {
+      username: {
+        required: true
+      },
+      firstname: {
+        required: true
+      },
+      lastname: {
+        required: true
+      },
+      email: {
+        required: true,
+        email: true
+      },
+      password: {
+        minlength: 6,
+        required: true
+      },
+      confirmation: {
+        minlength: 6,
+        equalTo: "#input-password"
+      }
+    }
+  });
 
-    $('#change-pass').validate({
-        rules: {
-            oldPasswordUser: {
-                minlength: 6,
-                required: true
-            },
-            newPasswordUser: {
-                minlength: 6,
-                required: true
-            },
-            newPasswordUserCf: {
-                minlength: 6,
-                required: true,
-                equalTo: "#input-NewPassword"
-            }
-        }
-    });
+  $('#change-pass').validate({
+    rules: {
+      oldPasswordUser: {
+        minlength: 6,
+        required: true
+      },
+      newPasswordUser: {
+        minlength: 6,
+        required: true
+      },
+      newPasswordUserCf: {
+        minlength: 6,
+        required: true,
+        equalTo: "#input-NewPassword"
+      }
+    }
+  });
 });
