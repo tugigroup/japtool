2bfe84d5bb74ba6f3805ade1269f81be905bd33c
unknown <TuanNT22@117TuanNT22.fsoft.fpt.vn>
2015-07-17 10:17:00 +0700

    Kanji text to speech function

---
+++ api/controllers/common/SpeechController.js
@@ -0,0 +1,15 @@
+/**
+ * Created by TuanNT22 on 17-07-2015.
+ */
+var tts = require('node-google-text-to-speech')
+module.exports = {
+  tts: function (req, res) {
+    var tl = req.param('tl');
+    var w = req.param('w');
+    tts.translate(tl, w, function (result) {
+      if (result.success) { //check for success
+        res.send({'audio': result.audio});
+      }
+    });
+  }
+}
