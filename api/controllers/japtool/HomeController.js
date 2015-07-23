a8376bf31ecb7b44b7f950fc500e3dcad88f49b5
unknown <DongDL1@117DongDL1.fsoft.fpt.vn>
2015-07-23 17:31:55 +0700

    Optimize controller

--- api/controllers/japtool/HomeController.js
+++ api/controllers/japtool/HomeController.js
@@ -2,6 +2,11 @@
  * Created by TuyenTV1 on 6/22/2015.
  */
 module.exports = {
+    _config: {
+        locals: {
+            layout: 'layout/layout-japtool'
+        }
+    },
 //This loads index.ejs
     index: function (req, res) {
         var user;
@@ -58,7 +63,30 @@ module.exports = {
             })
         })
 
-    }
+    },
+
+    getContent: function (req, res) {
+        try {
+            SelfLearning.find({user: req.session.User.id})
+                .populate('bookMaster')
+                .populate('userLearnHistories').exec(function (err, selfLearnings) {
+                    if (err) {
+                        sails.log("Err when read data from server:");
+                        return res.serverError(err);
+                    }
+                    //sails.log("Book Use History All.");
+                    //sails.log(selfLearnings);
+                    if (selfLearnings == null || selfLearnings == undefined) {
+                        return res.json({err: "Error"});
+                    }
+                    res.view('japtool/home/home', {selfLearnings: selfLearnings});
+                });
+        }
+        catch (ex) {
+            sails.log(ex);
+        }
+    },
+
 }
 
 /**
--- api/controllers/japtool/LearningController.js
+++ api/controllers/japtool/LearningController.js
@@ -7,37 +7,6 @@ module.exports = {
             layout: 'layout/layout-japtool'
         }
     },
-    home: function (req, res) {
-        try {
-            SelfLearning.find({user: req.session.User.id})
-                .populate('bookMaster')
-                .populate('userLearnHistories').exec(function (err, selfLearnings) {
-                    if (err) {
-                        sails.log("Err when read data from server:");
-                        return res.serverError(err);
-                    }
-                    //sails.log("Book Use History All.");
-                    //sails.log(selfLearnings);
-                    if (selfLearnings == null || selfLearnings == undefined) {
-                        return res.json({err: "Error"});
-                    }
-                    res.view('japtool/home/home', {selfLearnings: selfLearnings});
-                });
-        }
-        catch (ex) {
-            sails.log(ex);
-        }
-    },
-
-    missLesson: function (req, res) {
-        try {
-
-            res.view('japtool/home/missLesson');
-        }
-        catch (ex) {
-            sails.log(ex);
-        }
-    },
     /**
      * GET: japtool/learning/create
      * @param req
@@ -399,9 +368,4 @@ module.exports = {
         })
 
     },
-    _config: {
-        locals: {
-            layout: 'layout/layout-japtool'
-        }
-    }
 };
