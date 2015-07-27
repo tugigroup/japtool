c09d6ee86c2b8389845735cc187ceddf69008565
unknown <DuongTD2@117DuongTD2.fsoft.fpt.vn>
2015-07-27 11:04:17 +0700

    duongtd2 layout

--- api/controllers/japtool/KanjiController.js
+++ api/controllers/japtool/KanjiController.js
@@ -9,28 +9,28 @@ module.exports = {
     });
   },
   //Đây thực chất là hàm exercise nhưng vì chưa có link đến đây, nên để tạm là list để test
-  exercise: function (req, res) {
-    var extractDataCondition = req.param('condition');
-    Kanji.selectByLevel({condition: extractDataCondition}, function (err, kanjis) {
-      if (err) return res.send(err.status);
-      var min = 1;
-      var max = kanjis.length;
-      kanjis.forEach(function (item, index) {
-        var randomArr = [];
-        for (var i = 0; randomArr.length < 3; i++) {
-          var randomResult = Math.floor(Math.random() * (max - min) + min);
-          if (!(randomArr.indexOf(randomResult) > -1) && randomResult != index) {
-            randomArr[randomArr.length] = kanjis[randomResult].hanviet;
-          }
-        }
-        randomArr.push(item.hanviet);
-        randomArr.sort();
-        //console.log(item.hanviet, randomArr);
-        item.randomKanjis = randomArr;
-      });
-      res.render('japtool/kanji/exercise', {'kanjis': kanjis});
-    });
-  },
+  //list: function (req, res) {
+  //  var extractDataCondition = req.param('condition');
+  //  Kanji.selectByLevel({condition: extractDataCondition}, function (err, kanjis) {
+  //    if (err) return res.send(err.status);
+  //    var min = 1;
+  //    var max = kanjis.length;
+  //    kanjis.forEach(function (item, index) {
+  //      var randomArr = [];
+  //      for (var i = 0; randomArr.length < 3; i++) {
+  //        var randomResult = Math.floor(Math.random() * (max - min) + min);
+  //        if (!(randomArr.indexOf(randomResult) > -1) && randomResult != index) {
+  //          randomArr[randomArr.length] = kanjis[randomResult].hanviet;
+  //        }
+  //      }
+  //      randomArr.push(item.hanviet);
+  //      randomArr.sort();
+  //      //console.log(item.hanviet, randomArr);
+  //      item.randomKanjis = randomArr;
+  //    });
+  //    res.render('japtool/kanji/exercise', {'kanjis': kanjis});
+  //  });
+  //},
   _config: {
     locals: {
       layout: 'layout/layout-japtool'
