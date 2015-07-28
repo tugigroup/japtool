e483a96ed1788dc0acf07a569f17986f191f2c22
duluong <luongvandu@gmail.com>
2015-07-28 23:18:25 +0900

    localize

--- api/controllers/japtool/AuthController.js
+++ api/controllers/japtool/AuthController.js
@@ -69,6 +69,7 @@ module.exports = {
 
                 req.session.authenticated = true;
                 req.session.User = user;
+                req.session.lang = user.language;
 
                 //If the user is also an admin redirect to the user list(/view/user/index.ejs)
                 //This is user in conjuntion with config/policies.js file
