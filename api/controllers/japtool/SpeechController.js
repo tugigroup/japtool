18fbf7518c67c2694fb62884c0e2dbbe4eed8ee1
itplusvn <TuyenTV1@117TuyenTV1.fsoft.fpt.vn>
2015-07-30 16:47:46 +0700

    update

--- api/controllers/japtool/SpeechController.js
+++ api/controllers/japtool/SpeechController.js
@@ -2,11 +2,11 @@
  * Created by TuyenTV1 on 7/28/2015.
  */
 module.exports = {
-    speech: function (req, res) {
+    index: function (req, res) {
         var extractDataCondition = '{"level": "N4", "tag" : {"contains":",speedN4,lesson1,"}}';
         Sentence.selectByLevel({condition: extractDataCondition}, function (err, sentences) {
             if (err) return res.send(err.status);
-            res.view('japtool/sentence/speech', {'sentences': sentences});
+            res.view({'sentences': sentences});
         });
     },
     _config: {
