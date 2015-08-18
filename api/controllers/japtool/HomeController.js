c42405f5b28b39039b58c3e0ac957708b3b29fc5
TuyenTV1 <TuyenTV1@117TuyenTV1.fsoft.fpt.vn>
2015-08-18 16:57:11 +0700

    update

--- api/controllers/japtool/HomeController.js
+++ api/controllers/japtool/HomeController.js
@@ -24,7 +24,51 @@ module.exports = {
         })
 
     },
-    limitMasterBook: function (req, res) {
+    lessonHome: function (req, res) {
+        var bookMasterId = req.param('bookMasterId');
+        var selfLesson = req.param('selfLesson');
+        var moreSelfLesson = req.param('moreSelfLesson');
+        BookDetail.find({
+            bookMaster: bookMasterId,
+            sort: 'sort ASC',
+            subLesson: {'!': 'Từ vựng'}
+        }).exec(function (err, listItems) {
+            if (err) {
+                console.log(err);
+            } else {
+                if(!moreSelfLesson || moreSelfLesson == undefined) {
+                    UserLearnHistory.find({selfLearning: selfLesson}).exec(function (err, selfLesson) {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            res.render('japtool/home/lessonHome', {
+                                listItems: listItems,
+                                selfLesson: selfLesson
+                            });
+
+                        }
+                    })
+                }else{
+                    UserLearnHistory.find({selfLearning: moreSelfLesson}).exec(function (err, selfLesson) {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            res.render('japtool/home/limitLessonHome', {
+                                listItems: listItems,
+                                moreSelfLesson: selfLesson
+                            });
+
+                        }
+                    })
+                }
+            }
+
+
+
+
+        })
+    },
+    loadMoreIndex: function (req, res) {
         userId = req.session.User.id;
         var start = parseInt(req.param('start'));
         SelfLearning.find({
@@ -37,98 +81,10 @@ module.exports = {
                 if (err) return res.serverError(err);
             } else {
                 res.render('japtool/home/limitLesson', {
-                    listLessons: listLessons
+                    loadMorelistLessons: listLessons
                 });
             }
         })
 
-    }
-    ,
-    lessonHome: function (req, res) {
-        var bookMasterId = req.param('bookMasterId');
-        var selfLearning = req.param('selfLearning');
-        var moreBookMasterId = req.param('moreBookMasterId');
-        var moreSelfLearning = req.param('moreSelfLearning');
-        if (!moreBookMasterId || moreBookMasterId == undefined) {
-            var bookMasterIdL = bookMasterId.length;
-            var selfLearningL = selfLearning.length;
-        } else {
-            var moreBookMasterIdL = moreBookMasterId.length;
-            var moreSelfLearningL = moreSelfLearning.length;
-        }
-        var listItems_array = [];
-        var count = 0;
-        if (!moreBookMasterId || moreBookMasterId == undefined) {
-            for (i = 0; i < bookMasterIdL; i++) {
-                BookMaster.find({id: bookMasterId[i], sort: 'createdAt DESC'}).populate('bookDetails', {
-                    subLesson: {'!': 'Từ vựng'},
-                    sort: 'createdAt ASC'
-                }).exec(function (err, listItems) {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        count++;
-                        listItems_array.push(listItems);
-                        if (count == bookMasterIdL) {
-                            var countSelf = 0;
-                            var selfLesson_array = [];
-                            for (j = 0; j < selfLearningL; j++) {
-                                UserLearnHistory.find({selfLearning: selfLearning[j]}).exec(function (err, selfLesson) {
-                                    if (err) {
-                                        console.log(err);
-                                    } else {
-                                        countSelf++;
-                                        selfLesson_array.push(selfLesson);
-                                        if (countSelf == selfLearningL) {
-                                            res.render('japtool/home/lessonHome', {
-                                                listItems: listItems_array,
-                                                selfLesson:selfLesson_array,
-                                                selfLearning:selfLearning
-                                            });
-                                        }
-                                    }
-                                })
-                            }
-
-                        }
-                    }
-                })
-            }
-        } else {
-            for (i = 0; i < moreBookMasterIdL; i++) {
-                BookMaster.find({id: moreBookMasterId[i], sort: 'createdAt DESC'}).populate('bookDetails', {
-                    subLesson: {'!': 'Từ vựng'},
-                    sort: 'createdAt ASC'
-                }).exec(function (err, listItems) {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        count++;
-                        listItems_array.push(listItems);
-                        if (count == moreBookMasterIdL) {
-                            var moreCountSelf = 0;
-                            var moreSelfLesson_array = [];
-                            for (j = 0; j < moreSelfLearningL; j++) {
-                                UserLearnHistory.find({selfLearning: moreSelfLearning[j]}).exec(function (err, selfLesson) {
-                                    if (err) {
-                                        console.log(err);
-                                    } else {
-                                        moreCountSelf++;
-                                        moreSelfLesson_array.push(selfLesson);
-                                        if (moreCountSelf == moreSelfLearningL) {
-                                            res.render('japtool/home/limitLessonHome', {
-                                                listItems: listItems_array,
-                                                moreSelfLesson:moreSelfLesson_array
-                                            });
-                                        }
-                                    }
-                                })
-                            }
-
-                        }
-                    }
-                })
-            }
-        }
     }
 }
